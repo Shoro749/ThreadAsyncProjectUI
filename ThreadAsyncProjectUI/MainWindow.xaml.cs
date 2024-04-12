8750974380fe7740eac036f21661de74c2cd4946
Shoro749 <romanbro749@gmail.com>
2024-04-12 18:40:55 +0300

    Task completed

--- ThreadAsyncProjectUI/MainWindow.xaml.cs
+++ ThreadAsyncProjectUI/MainWindow.xaml.cs
@@ -11,11 +11,37 @@ using System.Windows.Shapes;
 
 namespace ThreadAsyncProjectUI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private bool start;
+        public MainWindow()
+        {
+            InitializeComponent();
+            start = false;
+            Thread thread = new Thread(Thread1);
+            thread.Start();
+        }
 
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (char.ToLower(e.Key.ToString()[0]).ToString() == "s")
+            {
+                start = !start;
+            }
+        }
+
+        private void Thread1()
+        {
+            Random rnd = new Random();
+            while (true)
+            {
+                Thread.Sleep(1000);
+                if (start == false) continue;
+                Dispatcher.Invoke(() =>
+                {
+                    textBlock.Text = rnd.Next().ToString();
+                });
+            }
+        }
     }
 }
