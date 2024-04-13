2123ac4e760aa20e2b765000207676b9612731bd
Shoro749 <romanbro749@gmail.com>
2024-04-13 18:14:04 +0300

    Task completed

--- ThreadAsyncProjectUI/MainWindow.xaml.cs
+++ ThreadAsyncProjectUI/MainWindow.xaml.cs
@@ -16,6 +16,57 @@ namespace ThreadAsyncProjectUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Horse1();
+            await Horse2();
+        }
+
+        private async Task Horse1()
+        {
+            while (horse1.Value < 100)
+            {
+                Random rnd = new Random();
+                int temp = rnd.Next(0, 20);
+                if (100 - horse1.Value < temp)
+                {
+                    horse1.Value = 100;
+                    if (horse2.Value != 100)
+                    {
+                        lb.Content = "Winner: Horse 1!";
+                    }
+                    break;
+                }
+                else { horse1.Value += temp; }
+
+                await Task.Delay(1000);
+            }
+        }
+
+        private async Task Horse2()
+        {
+            while (horse2.Value < 100)
+            {
+                Random rnd = new Random();
+                int temp = rnd.Next(0, 20);
+                if (100 - horse2.Value < temp)
+                {
+                    horse2.Value = 100;
+                    if (horse1.Value != 100)
+                    {
+                        lb.Content = "Winner: Horse 2!";
+                    }
+                    break;
+                }
+                else { horse2.Value += temp; }
+
+                await Task.Delay(1000);
+            }
+        }
     }
 }
