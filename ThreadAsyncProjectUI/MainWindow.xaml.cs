edfb4d6ee0049eb53712eb2120cd15e25f5cb752
Shoro749 <romanbro749@gmail.com>
2024-04-13 17:41:58 +0300

    Task completed

--- ThreadAsyncProjectUI/MainWindow.xaml.cs
+++ ThreadAsyncProjectUI/MainWindow.xaml.cs
@@ -16,6 +16,58 @@ namespace ThreadAsyncProjectUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private object locker = new object();
+        private int[] arr = new int[8];
+        public MainWindow()
+        {
+            InitializeComponent();
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(0, 100);
+            }
 
+            foreach (int i in arr)
+            {
+                Arr.Text += i.ToString() + " ";
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (tb.Text.All(char.IsDigit))
+            {
+                Sorter();
+                await Searcher();
+            }
+        }
+
+        private async Task Sorter()
+        {
+            lock(locker)
+            {
+                Array.Sort(arr);
+                result.Text += "Sorted array: ";
+                foreach (int i in arr)
+                {
+                    result.Text += i.ToString() + " ";
+                }
+            }
+        }
+
+        private async Task Searcher()
+        {
+            lock (locker)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == Convert.ToInt32(tb.Text))
+                    {
+                        result.Text += "\nIndex: " + i.ToString();
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
